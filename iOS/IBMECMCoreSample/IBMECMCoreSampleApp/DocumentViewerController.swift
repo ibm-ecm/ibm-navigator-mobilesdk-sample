37067844ef4a838ac1ef2998f365d628c5f24c2f
Qi Wang <wangqqi@cn.ibm.com>
2023-08-21 15:42:45 +0800

    Update copyright

--- iOS/IBMECMCoreSample/IBMECMCoreSampleApp/DocumentViewerController.swift
+++ iOS/IBMECMCoreSample/IBMECMCoreSampleApp/DocumentViewerController.swift
@@ -1,7 +1,12 @@
 /*
-* © Copyright IBM Corp. 2015
-*/
-
+ * Licensed Materials - Property of IBM
+ * (C) Copyright IBM Corporation 2015, 2023. All Rights Reserved.
+ * This sample program is provided AS IS and may be used, executed, copied
+ * and modified without royalty payment by customer (a) for its own instruction
+ * and study, (b) in order to develop applications designed to run with an IBM
+ * product, either for customer's own internal use or for redistribution by
+ * customer, as part of such an application, in customer's own products.
+ */
 import Foundation
 import UIKit
 import QuickLook
